2da9b7ce4fec8c7ca42cc63b04b139afbb299c88
Sazzad Hosen <mdsazzadhosentamim95@gmail.com>
2022-11-08 11:34:39 +0600

    added extra section in hom page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <footer className="footer footer-center p-10  bg-black text-white rounded">
   <div className="grid grid-flow-col gap-4">
     <a href='' className="link link-hover">About us</a> 
     <a className="link link-hover">Contact</a> 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import About1 from './About/About1';
+import About2 from './About/About2';
 import Slider from './Slider/Slider';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <About1></About1>
+            <About2></About2>
         </div>
     );
 };
