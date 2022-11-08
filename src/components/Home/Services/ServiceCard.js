1584a19b785f74b94ff03280ad4b94760387c7f8
Sazzad Hosen <mdsazzadhosentamim95@gmail.com>
2022-11-08 22:43:47 +0600

    added react photo view

--- src/components/Home/Services/ServiceCard.js
+++ src/components/Home/Services/ServiceCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 import { Link } from 'react-router-dom';
 
 const ServiceCard = ({service}) => {
     const {_id, img, price, title, description} = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure><img src={img} alt="Shoes" /></figure>
+       <PhotoProvider>
+        <PhotoView src={img} >
+        <figure><img src={img} alt="" /></figure>
+        </PhotoView>
+       </PhotoProvider>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
